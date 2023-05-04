797ac81ea9b134cc762467a40bd8f512a8559b4c
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-03 19:34:41 -0500

    Adding Section-04-Workspace files and directories

---
+++ Section-04-Workspace/Project-01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "This is Project 01" << std::endl;
+    
+    return 0;
+}
---
+++ Section-04-Workspace/Project-02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "This is Project 02" << std::endl;
+    
+    return 0;
+}
