52730fab9ba917d42d79d5402bfe273b4d648af8
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-10 07:09:10 -0500

    Updating 35-Section-Challenge-Solution files and directories

--- Section-04-Workspace/35-Section-Challenge-Solution/main.cpp
+++ Section-04-Workspace/35-Section-Challenge-Solution/main.cpp
@@ -35,7 +35,7 @@ int main(void) {
     
     int favorite_number = 0;
     
-    std::cout <<"Enter your favorite number: ";
+    std::cout <<"Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
     
     std::cout << "Amazing!! That's my favorite number, too!" << std::endl;
