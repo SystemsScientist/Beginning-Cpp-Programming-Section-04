8f2d4929ff87b1460e3f754130c90a7095329662
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-10 07:14:25 -0500

    Adding 35_section_challenge_solution_in_Python.py file

---
+++ 35_section_challenge_solution_in_Python.py
@@ -0,0 +1,24 @@
+# 
+# Author: Matt Johnson
+# Purpose: convert section challenge from C++ to Python
+# Subject: language translation
+# Date: 05/10/2023
+#
+# Notes:
+# 
+#   This program translates the section challenge solution
+#   from C++ to Python
+#
+#   To compile the file, use the following command-line
+#   Script:
+#
+#       $ python3 name_of_file.py
+#
+
+favorite_number = input("Enter your favorite number between 1 and 100: ")
+
+print("Amazing!! That's my favorite number, too!")
+print("No, really!!! " + favorite_number + " is my favorite number!")
+
+
+
