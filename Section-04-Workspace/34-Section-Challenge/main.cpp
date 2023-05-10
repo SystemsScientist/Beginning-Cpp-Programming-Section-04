bda32a860a50055bf15f18bf65909228d64205df
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-10 06:31:41 -0500

    Adding 34-Section-Challenge files and directories

---
+++ Section-04-Workspace/34-Section-Challenge/main.cpp
@@ -0,0 +1,41 @@
+/*
+ *  Author: Matt Johnson
+ *  Purpose: designing section challenge program
+ *  Subject: basic C++ program
+ *  Date: 05/09/2023 
+ * 
+ *  Notes:
+ * 
+*       Create a C++ program that asks the user for their favorite
+*       number between 1 and 100 then read this number from the
+*       console.
+* 
+*       Suppose the user enters 24.
+* 
+*       Then display the following to the console:
+* 
+*               Amazing!! That's my favorite number, too!
+*               No, really!!! 24 is my favorite number!
+* 
+*       Below are two sample runs of the program:
+*       -------------------------------------------------------------------------------------
+* 
+*       Enter your favorite number between 1 and 100: 24
+*       Amazing!! That's my favorite number, too!
+*       No, really!!! 24 is my favorite number!
+*     
+*       Enter your favorite number between 1 and 100: 75
+*       Amazing!! That's my favorite number, too!
+         No, really!!! 24 is my favorite number!
+ */
+
+#include <iostream>
+
+int main(void) {
+    
+    std::cout << "Instructions of Section Challenge. See" << std::endl;
+    std::cout << "35-Section-Challenge-Solution directory" << std::endl;
+    std::cout << "for solution.\n" << std::endl;
+    
+    return 0;
+}
