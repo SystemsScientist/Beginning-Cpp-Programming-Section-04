a84b668c75761f93c0654b0b166a4be262b72d92
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-13 06:25:33 -0500

    Adding 29_what_are_compiler_errors.cpp file

---
+++ Command-Line-Workspace/29_what_are_compiler_errors.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: Matt Johnson
+ * Subject: Compiler errors
+ * Purpose: exploring compiler errors
+ * Date: 05/13/2023
+ *
+ * Notes:
+ *
+ * 	1. remove right-hand quote to see syntax error
+ * 	2. add an additional "l" to endl to get syntax error
+ *
+ * 	3. remove semi-colon to see syntax error
+ * 	4. remove "0" from return statement to get syntax error
+ *
+ * 	5. replace "Matt" with "0" in return statement to get
+ * 	   semantic error
+ * 	6. divide "Hello, World!" by 125 to get semantic error
+ */
+
+#include <iostream>
+
+int main(void) {
+
+	std::cout << "Hellow, World!" << std::endl;
+
+	return 0;
+}
